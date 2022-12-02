cb41a88bad40d702db06e7313591a839ec677fcb
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2022-12-02 02:54:43 +0100

    Update sentiment_analyze.py

--- sentiment_analyze.py
+++ sentiment_analyze.py
@@ -25,7 +25,7 @@ file3 =  open('fit_tfidf1.pkl', 'rb')
 processor = dill.load(file1)
 predictor = joblib.load('new-model')
 fitted = dill.load(file3)
-X_train = np.load('XX_traiN')
+X_train = np.load('XX_traiN.npy')
 
 # Importing module
 
